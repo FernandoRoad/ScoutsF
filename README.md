275be4d11c4dacad452e3161c0fb420ef6f666d2
flamenquim <flamenquim@SVF1531A4E>
2018-04-20 01:53:32 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Configuracion y definicion del proyecto JPA para BD
+*Joaquin Terrasa Moya <quino.terrasa+dev@gmail.com>*
+*11 Abril 2018*
+
+### Configuracion del archivo *persistence.xml*
+
+### Configuracion de la BD externa
+
+### Como compilar y lanzar el proyecto
+
+#### Apendice: Archivos generados
+
+#### Apendice: Errores comunes
---
+++ src/scouts/Coordinador.java
@@ -0,0 +1,50 @@
+package scouts;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Rafael Martin Carrion, Joaquin Terrasa Moya
+ */
+@Entity
+public class Coordinador extends Usuario implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Column(nullable = false, length = 40)
+    private String CorreoContacto;
+    
+    @Column(nullable = false, length = 9)
+    private String DNI;
+    
+    @Column(nullable = false, length = 12)
+    private Long TelefonoContacto;
+
+    //! Getters y Setters
+    
+    public Long getTelefonoContacto() {
+        return TelefonoContacto;
+    }
+
+    public void setTelefonoContacto(Long TelefonoContacto) {
+        this.TelefonoContacto = TelefonoContacto;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getCorreoContacto() {
+        return CorreoContacto;
+    }
+
+    public void setCorreoContacto(String CorreoContacto) {
+        this.CorreoContacto = CorreoContacto;
+    }
+}
